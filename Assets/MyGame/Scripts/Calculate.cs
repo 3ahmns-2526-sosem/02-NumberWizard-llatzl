da6c5714e7b15745e1f6b28c145335fb4602bbd0
lillilatzl@xy <lillilatzl@xy>
2026-06-10 13:58:59 +0200

    calculate first guess

---
+++ Assets/MyGame/Scripts/Calculate.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using TMPro; // Nötig, wenn du TextMeshPro für die UI verwendest
+
+public class Calculate : MonoBehaviour
+{
+    [Header("Zahlenbereich")]
+    [SerializeField] private int min = 1;
+    [SerializeField] private int max = 100;
+
+    [Header("UI Elemente")]
+    [SerializeField] private TMP_Text guessText; 
+
+    private int guess;
+
+    void Start()
+    {
+        CalculateFirstGuess();
+    }
+
+    void CalculateFirstGuess()
+    {
+        
+        guess = (min + max) / 2;
+
+        // Aktualisierung der UI
+        UpdateGuessUI();
+    }
+
+    void UpdateGuessUI()
+    {
+        if (guessText != null)
+        {
+            guessText.text = guess.ToString();
+        }
+        else
+        {
+            Debug.LogWarning("flasch");
+        }
+    }
+}
